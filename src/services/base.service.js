7aee1d30431767a1f97d6de599d595e2bf7cb88b
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2024-06-27 16:29:16 -0500

    Update base.service.js

--- src/services/base.service.js
+++ src/services/base.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const ApiUrl = 'http://20.51.216.44/'
+export const ApiUrl = 'https://20.51.216.44/'
 
 const BaseService = axios.create ({
     baseURL: ApiUrl,
